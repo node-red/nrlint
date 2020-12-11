12f95585769266f8da0f6f6112abdc92fa1f5eec
Kunihiko Toumura <kunihiko.toumura.yv@hitachi.com>
2020-12-11 15:59:59 +0900

    add test cases

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,21 @@ module.exports = function(grunt) {
             }
         },
         eslint: {
-            target: [ 'bin/*.js', 'lib/*.js', 'test/**/*_spec.js' ]
+            target: [ 'bin/**/*.js', 'lib/**/*.js', 'test/**/*_spec.js' ]
+        },
+        jsdoc: {
+            dist: {
+                src: [ 'bin/**/*.js', 'lib/**/*.js' ],
+                options: {
+                    destination: 'doc',
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-mocha-istanbul');
     grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.registerTask('default', ['eslint']);
     grunt.registerTask('test', ['simplemocha']);
     grunt.registerTask('coverage', 'Run Instanbul code test coverage task', [ 'mocha_istanbul:all' ]);
